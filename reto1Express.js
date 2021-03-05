aa3b033fcd0373f09073c27a963f68cb07af03d6
NataliSb9 <nsainzbarroso@gmail.com>
2021-03-05 06:47:15 +0100

    proyecto servidor expres ya corregido

--- reto1Express.js
+++ reto1Express.js
@@ -37,59 +37,205 @@ app.use(express.json());
             console.log("****************************");
             }
         }
-    let profesionalClase = new Professional("Tim Robbins", 62, "Masculino", 70, 1.75, "Castanyo", "Verde", "Blanco", false, "estadounidense", 3, "actor") 
     
+
     let profesional = null;
-    
-    
-    app.post("/profesional",
-                function( request,response){
-                    let respuesta;
-                    if (profesional === null)
-                    {
-                        profesional = {name: request.body.name}
+    /*let profesional = new Professional("Tim Robbins", 62, "Masculino", 70, 1.75, "Canoso", "Verde", "Blanco", false, "estadounidense", 3, "actor");*/
 
-                        respuesta =  {error: true, codigo: 200,
-                            mensaje: 'Profesional creado', resultado: profesional
-                        }
-                    }
-                    else {
-                        respuesta= { error: true, codigo: 200,
-                            mensaje: 'Profesional ya existe', resultado: null};
-                    }
-                        
 
-                    response.send(respuesta)
-                } );
+let respuesta;
     
-    app.put("/profesional",
-            function(request, response)
-            {
-               let respuesta
-               if (profesional != null)
-               {
-                   profesional.name = request.body.name;
-                   //profesional.age = request.body.age;
-                   respuesta = { error: false, codigo: 200, mensaje: "Profesional actualizado", resultado: profesional}
-               } 
-               else
-               respuesta = {error: true, codigo: 200, mensaje: "El profesional no existe", resultado: profesional}
+app.get("/profesional", function (request, response){
+        
+    if  (profesional != null){
 
-               response.send(respuesta)
-            });
-    app.delete("/profesional",
-            function(request, response)
-            {
-                let respuesta
-                if (profesional != null)
-                {
-                    profesional = null;
-                    respuesta = {error: false, codigo:200, mensaje: "Profesional borrado", respuesta: profesional};
-                }
-                else
-                    respuesta = {error: true, codigo: 200,
-                    mensaje: 'Profesional borrado', resultado: profesional};
-                response.send(respuesta);
-            });
+        respuesta = profesional;
+
+    } else {
+        respuesta = {
+            error: true, 
+            codigo: 200, 
+            mensaje: 'No hay profesional'
+        }
+    }
     
+    response.send(respuesta)
+});
+
+
+app.post("/profesional",function( request,response){
+
+    if (profesional === null)
+    {
+        profesional = { 
+            name: request.body.name,
+            nationality: request.body.nationality,
+            profession: request.body.profession,
+            genre: request.body.genre,
+            age: request.body.age,
+            weight: request.body.weight,
+            height: request.body.height,
+            hairColor: request.body.hairColor,
+            eyeColor: request.body.eyeColor,
+            race: request.body.race,
+            isRetired: request.body.isRetired,
+        }
+
+        respuesta =  {
+            error: true, 
+            codigo: 200,
+            mensaje: 'Profesional creado', 
+            resultado: profesional,
+        }
+    }
+    else {
+        respuesta= { 
+            error: true, 
+            codigo: 200,
+            mensaje: 'Profesional ya existe', 
+            resultado: null
+        }
+    }
+    response.send(respuesta)
+});
+    
+app.put("/profesional",
+
+function(request, response){ 
+    
+    if (profesional != null){
+        
+        if (request.body.name != null){
+            profesional.name = request.body.name; 
+            respuesta = {
+                error: true, 
+                codigo: 200, 
+                mensaje: "El profesional actualizado", 
+                resultado: profesional
+            }
+        }    
+
+        if(request.body.nationality != null){
+            profesional.nationality = request.body.nationality;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }
+        }
+        
+        if(request.body.oscarsNumber != null){
+            profesional.oscarsNumber = request.body.oscarsNumber;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+        if(request.body.hairColor != null){
+            profesional.hairColor = request.body.hairColor;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+        if(request.body.oscarsNumber != null){
+            profesional.race = request.body.race;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+        if(request.body.age != null){
+            profesional.age = request.body.age; 
+            respuesta = {
+                error: true, 
+                codigo: 200, 
+                mensaje: "El profesional actualizado", 
+                resultado: profesional
+            }
+        }    
+
+        if (request.body.profesional != null) 
+            {profesional.name = request.body.profesional; 
+            respuesta = {
+                error: true, 
+                codigo: 200, 
+                mensaje: "El profesional actualizado", 
+                resultado: profesional
+            }            
+        }   
+
+        if(request.body.isRetired != null){
+            profesional.isRetired = request.body.isRetired;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+        if(request.body.isRetired != null){
+            profesional.weight = request.body.weight;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+        if(request.body.isRetired != null){
+            profesional.height = request.body.height;
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "El profesional actualizado",
+                resultado: profesional
+            }        
+        }
+
+    }else {
+            respuesta = 
+            {error: true, 
+            codigo: 200, 
+            mensaje: "El profesional no existe",
+            resultado: profesional
+        }
+    }
+
+    response.send(respuesta)
+});
+ 
+app.delete("/profesional",function(request, response){
+    let respuesta
+    if (profesional != null){
+        profesional = null;
+        respuesta = {
+            error: false, 
+            codigo:200, 
+            mensaje: "Profesional borrado", 
+            respuesta: profesional
+        }
+    }
+    else
+        respuesta = {
+            error: true, 
+            codigo: 200,
+            mensaje: 'Profesional no existe', 
+            resultado: profesional
+        }
+    
+    response.send(respuesta);
+});
 app.listen(3000);
